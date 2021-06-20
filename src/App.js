ab7caf5109b73a3d51c96fded86e792a24a008e4
implodingduck <zsek320@gmail.com>
2021-06-19 21:42:25 -0500

    adding a way to preview

--- src/App.js
+++ src/App.js
@@ -8,18 +8,29 @@ import Editor from './Editor'
 import {
   HashRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from "react-router-dom";
 
 function App() {
+  
+  const storyjson = (localStorage.getItem('editorstory')) ? JSON.parse(localStorage.getItem('editorstory')) : dragon 
+
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route path="/editor">
+            <Link to={"/preview"}>Preview</Link>
             <Editor ></Editor>
           </Route>
+          <Route path="/preview">
+            <Link to={"/editor"}>Edit</Link><br />
+            <button onClick={ () => window.location.reload() }>Refresh</button>
+            <Story story={storyjson.story}></Story>
+          </Route>
           <Route path="/:flowhash?">
+            <Link to={"/editor"}>Play around with your own story...</Link>
             <Story story={dragon.story}></Story>
           </Route>
         </Switch>
